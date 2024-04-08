1ec21aedf6efbbcd7db70331a83c69313dfc4ee6
fgael <gaelfoini@gmail.com>
2024-04-08 17:46:12 +0200

    fix color chart

--- src/components/common/CoinChart.vue
+++ src/components/common/CoinChart.vue
@@ -27,6 +27,7 @@ import {
   Filler,
 } from "chart.js";
 import { Line } from "vue-chartjs";
+import { cp } from "fs";
 
 ChartJS.register(
   CategoryScale,
@@ -43,8 +44,6 @@ const entryProps = defineProps<{
 }>();
 
 const isLoading = ref(true);
-const chartLineColor = ref("rgba(60, 179, 113, 1)");
-const chartBackgroundColor = ref("rgba(60, 179, 113, 1)");
 
 // Graph data
 const chartData = computed(() => {
@@ -144,15 +143,18 @@ const firstSparklineValue = computed(() => {
   }
 });
 
+const chartLineColor = computed(() => {
+  return lastSparklineValue.value > firstSparklineValue.value
+    ? "#46FF00"
+    : "#FF2700";
+});
+
+const chartBackgroundColor = computed(() => {
+  return lastSparklineValue.value > firstSparklineValue.value
+    ? "rgba(60, 179, 113, 0.2)"
+    : "rgba(255, 0, 0, 0.2)";
+});
 onMounted(async () => {
-  chartLineColor.value =
-    lastSparklineValue.value > firstSparklineValue.value
-      ? "#46FF00"
-      : "#FF2700";
-  chartBackgroundColor.value =
-    lastSparklineValue.value > firstSparklineValue.value
-      ? "rgba(60, 179, 113, 0.2)"
-      : "rgba(255, 0, 0, 0.2)";
   isLoading.value = false;
 });
 </script>
